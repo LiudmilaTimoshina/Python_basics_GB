86d2d154a402a6501e4278dcd940623d9e7d7a93
LiudmilaTimoshina <liudmila.timoshina@gmail.com>
2021-12-17 11:54:18 +0200

    еще один варинат решения задачи 3_5

---
+++ Timoshina Liudmila_dz_3/task_3_5_v2.py
@@ -0,0 +1,20 @@
+# Реализовать функцию get_jokes(), возвращающую n шуток, сформированных из трех случайных слов, взятых из трёх списков (по одному из каждого)
+
+nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
+adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
+adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
+
+from random import sample
+
+def get_jokes_2(n):
+    jokes = []
+    nn_list = sample(nouns,n)
+    adv_list = sample(adverbs,n)
+    adj_list = sample(adjectives,n)
+    for nn, adv, adj  in zip(nn_list, adv_list, adj_list):
+        jokes += f'{nn} {adv} {adj}'
+
+get_jokes_2(3)
+
+
+
